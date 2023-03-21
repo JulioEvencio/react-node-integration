9f3806c7ee6344dbf6f766e3c7bb060d0d9c7a59
JulioEvencio <julioevencio@acad.ifma.edu.br>
2023-03-20 22:24:44 -0300

    .gitignore and cors

--- back-end/index.js
+++ back-end/index.js
@@ -1,8 +1,10 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const port = 5000
 
 app.use(express.json())
+app.use(cors())
 
 let news = [
 	{id: '1', title: 'News 1', slug: 'new-1', body: 'My body 1'},
